4e11220d86a4c8deeeb3e2a7710e5c380d8bcc0f
Patrick <pmcgonigle@gmail.com>
2022-04-28 20:55:58 -0500

    TEXTBAK-2 Initial Xcode-generated project, untouched

---
+++ iMessageBackup/iMessageBackup/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iMessageBackup
+//
+//  Created by Patrick McGonigle on 4/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
