656f8e29458ec5bf2e2cdf3df871eecb3642df50
patmcgtx <57506933+patmcgtx@users.noreply.github.com>
2022-04-30 12:02:15 -0500

    TEXTBAK-12 Query the number of messages (#1)
    * TEXTBAK-12 Progress but need to finish xcdatamodeld
    
    * TEXTBAK-12 Updated the data model, but the db can't be loaded by the app
    
    * TEXTBAK-12 Removed CoreData
    
    * TEXTBAK-12 Added SQLite.swift package
    
    * TEXTBAK-12 Prompt the user to select the chat.db
    
    * TEXTBAK-12 Progress, can read a local copy of chat.db
    
    * TEXTBAK-12 Cleanup, refactor, docs
    
    * TEXTBAK-12 More cleanup
    
    * TEXTBAK-12 Basic user case complete
    
    * TEXTBAK-12 Small comment fixes

---
+++ iMessageBackup/iMessageBackup/UI/ChatDbFinder.swift
@@ -0,0 +1,46 @@
+//
+//  ChatDbSelector.swift
+//  iMessageBackup
+//
+//  Created by Patrick McGonigle on 4/29/22.
+//
+
+import AppKit
+import UniformTypeIdentifiers
+
+/// Provides a way to find the chat database file.
+struct ChatDbFinder {
+    
+    /**
+     Opens a dialog for the user to find and pick the chat database.
+     
+     Due to app sandboxing, we have to prompt the user to manually pick the `chat.db` file;
+     we can't just go grab it.  I guess this makes sense security-wise. 🤷🏻‍♂️
+     
+     - Returns: A URL with the location of the selected database file, or `nil` if none.
+     */
+    func promptForChatDb() -> URL? {
+        
+        let openPanel = NSOpenPanel();
+        
+        openPanel.title = "Locate chat.db";
+        openPanel.showsResizeIndicator = true;
+        openPanel.showsHiddenFiles = true;
+        openPanel.allowsMultipleSelection = false;
+        openPanel.canChooseDirectories = false;
+        openPanel.resolvesAliases = true
+        openPanel.isExtensionHidden = false
+        openPanel.canCreateDirectories = false
+        openPanel.allowedContentTypes = [UTType(filenameExtension: ".db"),
+                                         UTType(filenameExtension: ".sqlite"),
+                                         UTType(filenameExtension: ".sqlite3e")].compactMap{$0}
+        openPanel.allowsOtherFileTypes = false
+        
+        if (openPanel.runModal() ==  NSApplication.ModalResponse.OK) {
+            return openPanel.url
+        } else {
+            return nil
+        }
+    }
+    
+}
--- iMessageBackup/iMessageBackup/ViewController.swift
+++ iMessageBackup/iMessageBackup/ViewController.swift
@@ -8,11 +8,32 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var statusLabel: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    private func updateStatus(_ status: String) {
+        self.statusLabel.stringValue = status
+    }
+    
+    override func viewDidAppear() {
+        
+        // TODO Trigger this with a button
+        
+        if let dbURL = ChatDbFinder().promptForChatDb() {
+            let chatReader = ChatReader(dbPath: dbURL.path)
+            switch chatReader?.numMessages {
+            case .success(let count):
+                self.updateStatus("Found \(count) messages.")
+            case .failure(let error):
+                self.updateStatus(error.localizedDescription)
+            case .none:
+                self.updateStatus("Can't read database.")
+            }
+        }
     }
 
     override var representedObject: Any? {
@@ -20,7 +41,6 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
 
 }
 
